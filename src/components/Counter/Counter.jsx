9fc239380440cc7c298bb5c564e479da3f3b2630
OlenaLotos <olenameshanych@gmail.com>
2022-08-15 21:12:45 +0300

    start counter

---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Counter.css';
+
+const Counter = () => {
+  <div className="Counter">
+    <span className="Counter__value">0</span>
+
+    <div className="Counter__controls">
+      <button type="button">Add 1</button>
+      <button type="button">Substract 1</button>
+    </div>
+  </div>;
+};
+export default Counter;
