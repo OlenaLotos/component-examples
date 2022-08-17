307cf7b16729fb3c82ee928963f7cbbe45d3d534
OlenaLotos <olenameshanych@gmail.com>
2022-08-17 19:41:17 +0300

    form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,28 +3,34 @@ import React, { Component } from 'react';
 // import Dropdown from './Dropdown/Dropdown';
 // import TodoList from './TodosList/TodosList';
 // import initialTodos from './todos.json';
+import Form from './Form/Form';
 
 class App extends Component {
   state = {
-    // todos: initialTodos,
+    // todos: initialTodos,  //-------------- data for TODO component//
   };
 
+  //---------------------------------- data for TODO component//
   deleteTodo = todoId => {
     this.setState(prevState => ({
       todos: prevState.todos.filter(todo => todo.id !== todoId),
     }));
   };
+  //---------------------------------- data for TODO component//
+
+  formSubmitHandler = data => {
+    console.log(data);
+  };
 
   render() {
-    const { todos } = this.state;
-
-    const totalTodoCount = todos.length;
-    const completedTodosCount = todos.reduce(
-      (acc, todo) => (todo.completed ? acc + 1 : acc),
-      0
-    );
-
-    // console.log(completedTodosCount);
+    //---------------------------------- data for TODO component//
+    // const { todos } = this.state;
+    // const totalTodoCount = todos.length;
+    // const completedTodosCount = todos.reduce(
+    //   (acc, todo) => (todo.completed ? acc + 1 : acc),
+    //   0
+    // );
+    //---------------------------------- data for TODO component//
 
     return (
       <div
@@ -37,14 +43,17 @@ class App extends Component {
           color: '#010101',
         }}
       >
+        <Form onSubmit={this.formSubmitHandler} />
+
         {/* <Counter /> */}
+
         {/* <Dropdown /> */}
 
         {/* <TodoList todos={todos} onDeleteTodo={this.deleteTodo} /> */}
-
         {/* <div> */}
         {/* <span>Total number of Todos: {totalTodoCount} </span> */}
         {/* <span>Number of completed Todos: {completedTodosCount} </span> */}
+
         {/* </div> */}
       </div>
     );
