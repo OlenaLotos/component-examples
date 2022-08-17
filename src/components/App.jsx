fa08c2f1020b7778bfe88e6a8ca5ed5b27c0639b
OlenaLotos <olenameshanych@gmail.com>
2022-08-17 15:20:26 +0300

    counter todo dropdowwn

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,52 @@
-import React from 'react';
-import Counter from './Counter/Counter';
+import React, { Component } from 'react';
+// import Counter from './Counter/Counter';
+// import Dropdown from './Dropdown/Dropdown';
+import TodoList from './TodosList/TodosList';
+import initialTodos from './todos.json';
 
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      <Counter />
-    </div>
-  );
-};
+class App extends Component {
+  state = {
+    todos: initialTodos,
+  };
+
+  deleteTodo = todoId => {
+    this.setState(prevState => ({
+      todos: prevState.todos.filter(todo => todo.id !== todoId),
+    }));
+  };
+
+  render() {
+    const { todos } = this.state;
+
+    const totalTodoCount = todos.length;
+    const completedTodosCount = todos.reduce(
+      (acc, todo) => (todo.completed ? acc + 1 : acc),
+      0
+    );
+
+    // console.log(completedTodosCount);
+
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        {/* <Counter /> */}
+        {/* <Dropdown /> */}
+
+        <div>
+          <span>Total number of Todos: {totalTodoCount} </span>
+          <span>Number of completed Todos: {completedTodosCount} </span>
+        </div>
+        <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
+      </div>
+    );
+  }
+}
+export default App;
--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,16 +1,40 @@
 import React from 'react';
 import './Counter.css';
+import Controls from './Controls';
 
-const Counter = () => {
-  return (
-    <div className="Counter">
-      <span className="Counter__value">0</span>
+class Counter extends React.Component {
+  static defaultProps = {
+    initialValue: 0,
+  };
+  state = {
+    value: this.props.initialValue,
+  };
 
-      <div className="Counter__controls">
-        <button type="button">+ 1</button>
-        <button type="button">- 1</button>
+  handleIncrement = () => {
+    // this.setState({value: 123112}) як обєкт - коли перезаписуємо значення, як функція - коли відштовхуємось від попереднього значення і його змінюємо відповідно.
+    this.setState(prevState => ({
+      value: prevState.value + 1,
+    }));
+    // console.log('click on button +1');
+  };
+  handleDecrement = () => {
+    this.setState(prevState => ({
+      value: prevState.value - 1,
+    }));
+    // console.log('click on button -1');
+  };
+  render() {
+    return (
+      <div className="Counter">
+        <span className="Counter__value">{this.state.value}</span>
+
+        <Controls
+          onIncrement={this.handleIncrement}
+          onDecrement={this.handleDecrement}
+        />
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
 export default Counter;
