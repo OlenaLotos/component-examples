d6294233795189168bd859c7db1e45e83ce40435
OlenaLotos <olenameshanych@gmail.com>
2022-08-18 16:05:25 +0300

    todo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,61 +1,107 @@
 import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
 // import Counter from './Counter/Counter';
 // import Dropdown from './Dropdown/Dropdown';
-// import TodoList from './TodosList/TodosList';
-// import initialTodos from './todos.json';
-import Form from './Form/Form';
+import Container from './Container/Container';
+import TodoList from './TodosList/TodosList';
+import TodoEditor from './TodoEditor/TodoEditor';
+import Filter from './Filter/Filter';
+import initialTodos from './todos.json';
+// import Form from './Form/Form';
 
 class App extends Component {
   state = {
-    // todos: initialTodos,  //-------------- data for TODO component//
+    todos: initialTodos, //-------------- data for TODO component//
+    filter: '', //-------------- data for TODO component//
   };
 
   //---------------------------------- data for TODO component//
+  addTodo = text => {
+    const todo = {
+      id: nanoid(),
+      text,
+      completed: false,
+    };
+
+    this.setState(({ todos }) => ({
+      todos: [todo, ...todos],
+    }));
+  };
+
   deleteTodo = todoId => {
     this.setState(prevState => ({
       todos: prevState.todos.filter(todo => todo.id !== todoId),
     }));
   };
+
+  toggleCompleted = todoId => {
+    this.setState(({ todos }) => ({
+      todos: todos.map(todo =>
+        todo.id === todoId ? { ...todo, completed: !todo.completed } : todo
+      ),
+    }));
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getVisibleTodos = () => {
+    const { filter, todos } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return todos.filter(todo =>
+      todo.text.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  calculateCompletedTodos = () => {
+    const { todos } = this.state;
+
+    return todos.reduce(
+      (total, todo) => (todo.completed ? total + 1 : total),
+      0
+    );
+  };
   //---------------------------------- data for TODO component//
 
+  //---------------------------------- data for FORM component//
   formSubmitHandler = data => {
     console.log(data);
   };
+  //---------------------------------- data for FORM component//
 
   render() {
     //---------------------------------- data for TODO component//
-    // const { todos } = this.state;
-    // const totalTodoCount = todos.length;
-    // const completedTodosCount = todos.reduce(
-    //   (acc, todo) => (todo.completed ? acc + 1 : acc),
-    //   0
-    // );
+    const { todos, filter } = this.state;
+    const totalTodoCount = todos.length;
+    const completedTodoCount = this.calculateCompletedTodos();
+    const visibleTodos = this.getVisibleTodos();
     //---------------------------------- data for TODO component//
 
     return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <Form onSubmit={this.formSubmitHandler} />
+      <Container>
+        {/* <Form onSubmit={this.formSubmitHandler} /> */}
 
         {/* <Counter /> */}
 
         {/* <Dropdown /> */}
 
-        {/* <TodoList todos={todos} onDeleteTodo={this.deleteTodo} /> */}
-        {/* <div> */}
-        {/* <span>Total number of Todos: {totalTodoCount} </span> */}
-        {/* <span>Number of completed Todos: {completedTodosCount} </span> */}
+        <div>
+          <p>Total Todos: {totalTodoCount}</p>
+          <p>Done Todos: {completedTodoCount}</p>
+        </div>
 
-        {/* </div> */}
-      </div>
+        <TodoEditor onSubmit={this.addTodo} />
+
+        <Filter value={filter} onChange={this.changeFilter} />
+
+        <TodoList
+          todos={visibleTodos}
+          onDeleteTodo={this.deleteTodo}
+          onToggleCompleted={this.toggleCompleted}
+        />
+      </Container>
     );
   }
 }
