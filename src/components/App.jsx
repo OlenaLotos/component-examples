dc2bec55f894464352463b31ee66d24d82cc5d05
OlenaLotos <olenameshanych@gmail.com>
2022-08-19 12:37:16 +0300

    productreview form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,13 @@ import { nanoid } from 'nanoid';
 // import Counter from './Counter/Counter';
 // import Dropdown from './Dropdown/Dropdown';
 import Container from './Container/Container';
-import TodoList from './TodosList/TodosList';
-import TodoEditor from './TodoEditor/TodoEditor';
-import Filter from './Filter/Filter';
+// import TodoList from './TodosList/TodosList';
+// import TodoEditor from './TodoEditor/TodoEditor';
+// import Filter from './Filter/Filter';
 import initialTodos from './todos.json';
 // import Form from './Form/Form';
+// import FormLogin from './FormLogin/FormLogin';
+import { ProductReviewForm } from './ProductReviewForm/ProductReviewForm';
 
 class App extends Component {
   state = {
@@ -73,34 +75,31 @@ class App extends Component {
 
   render() {
     //---------------------------------- data for TODO component//
-    const { todos, filter } = this.state;
-    const totalTodoCount = todos.length;
-    const completedTodoCount = this.calculateCompletedTodos();
-    const visibleTodos = this.getVisibleTodos();
+    // const { todos, filter } = this.state;
+    // const totalTodoCount = todos.length;
+    // const completedTodoCount = this.calculateCompletedTodos();
+    // const visibleTodos = this.getVisibleTodos();
     //---------------------------------- data for TODO component//
 
     return (
       <Container>
         {/* <Form onSubmit={this.formSubmitHandler} /> */}
-
         {/* <Counter /> */}
-
         {/* <Dropdown /> */}
-
-        <div>
+        {/* <FormLogin />, */}
+        <ProductReviewForm />
+        {/* <div>
           <p>Total Todos: {totalTodoCount}</p>
           <p>Done Todos: {completedTodoCount}</p>
-        </div>
+        </div> */}
 
-        <TodoEditor onSubmit={this.addTodo} />
-
-        <Filter value={filter} onChange={this.changeFilter} />
-
-        <TodoList
-          todos={visibleTodos}
-          onDeleteTodo={this.deleteTodo}
-          onToggleCompleted={this.toggleCompleted}
-        />
+        {/* <TodoEditor onSubmit={this.addTodo} /> */}
+        {/* <Filter value={filter} onChange={this.changeFilter} /> */}
+        {/* <TodoList */}
+        {/* todos={visibleTodos} */}
+        {/* onDeleteTodo={this.deleteTodo} */}
+        {/* onToggleCompleted={this.toggleCompleted} */}
+        {/* /> */}
       </Container>
     );
   }
